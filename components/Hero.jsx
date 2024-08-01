a2ae617e577d7e72da796b31e75a27ba9c4aafc0
Atharva Sable <126801678+AtharvaMSable@users.noreply.github.com>
2024-08-01 21:07:59 +0530

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -44,7 +44,7 @@ const Hero = () => {
               <Button variant="secondary" className="gap-x-2">
                 Download CV <Download size={18} />
               </Button>
-              </Link>
+              </Link> 
             {/* Socials*/}
             <Socials
               containerStyles="flex gap-x-6 mx-auto xl:mx-0"
