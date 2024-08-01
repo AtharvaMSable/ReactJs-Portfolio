2f63698075b8139cf84ddd96988d00f48547b3bb
Atharva Sable <126801678+AtharvaMSable@users.noreply.github.com>
2024-08-01 21:04:05 +0530

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -39,11 +39,12 @@ const Hero = () => {
                 <Button className="gap-x-2">
                   Contact Me <Send size={18} />
                 </Button>
+                </Link>
               <Link href="https://drive.google.com/file/d/1SSHvhEjpDXONaAqzZOcgNSfmeo5v-6y2/view?usp=drive_link">
               <Button variant="secondary" className="gap-x-2">
                 Download CV <Download size={18} />
               </Button>
-            </div>
+              </Link>
             {/* Socials*/}
             <Socials
               containerStyles="flex gap-x-6 mx-auto xl:mx-0"
